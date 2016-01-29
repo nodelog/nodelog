767b7d6c1b7051cd646fc121a7f8637f4f5bab98
wyc0991 <wangyachao0991@sina.cn>
2016-01-30 00:39:07 +0800

    notes for app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//require
+//require加载库
 var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
@@ -10,51 +10,50 @@ var settings = require('./settings');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var routes = require('./routes/index');
+
+//初始化express框架
 var app = express();
+//设置view层页面路径
 app.set('views', path.join(__dirname, 'views'));
+//设置view层使用的模板引擎
 app.set('view engine', 'ejs');
 
-
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(compress());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon());//网站图标
+app.use(logger('dev'));//日志级别
+app.use(compress());//压缩
+app.use(bodyParser.json());//json
+app.use(bodyParser.urlencoded());//url编码
+app.use(cookieParser());//cookie组件
+app.use(express.static(path.join(__dirname, 'public')));//静态资源路径，
+//session配置，这里使用mongodb作为session存储
 app.use(session({
     secret: settings.cookieSecret,
     store: new MongoStore({
         db: settings.db
     })
 }));
+//接受请求
 app.use(function (req, res, next) {
     res.locals.session = req.session;
     next();
 });
+//装载路由
 routes.route(app);
 
-/// catch 404 and forward to error handler
+//404错误处理
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
+//环境设置和服务器500错误处理
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.redirect('/error');
     });
 }
-
-// production error handler
-// no stacktraces leaked to user
+// 500错误页面
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.redirect('/error');
@@ -65,13 +64,10 @@ app.use(function (err, req, res, next) {
     });*/
 });
 
-
 var debug = require('debug')('my-application'); // debug模块
 module.exports = app;
 
 app.set('port', process.env.PORT || settings.port); // 设定监听端口
-
-// Environment sets...
 
  //module.exports = app; //这是 4.x 默认的配置，分离了 app 模块,将它注释即可，上线时可以重新改回来
 
