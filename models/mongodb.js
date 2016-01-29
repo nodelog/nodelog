47bc53c2597d0b3ae5efc466d5bd5a0d392650a5
wyc0991 <wangyachao0991@sina.cn>
2016-01-30 01:19:14 +0800

    notes for mongodb

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
-var settings = require('../settings');
-mongoose.connect(settings.url);
-exports.mongoose = mongoose;
+var mongoose = require('mongoose');//引入mongoose模块实现mongodb的连接和数据库操作
+var settings = require('../settings');//引入配置
+mongoose.connect(settings.url);//连接mongodb
+exports.mongoose = mongoose;//导出mongodb的连接器mongoose
