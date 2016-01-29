9dd65cac97b094f02957bb2a52d96ae924fbd13f
wyc0991 <wangyachao0991@sina.cn>
2016-01-29 16:51:51 +0800

    config responsive

--- js/common.js
+++ js/common.js
@@ -867,5 +867,17 @@ $(function () {
 });
 
 <!--bs自适应导航插件-->
-var navigation = responsiveNav("#nav");
+var navigation = responsiveNav("#nav", { // Selector: The ID of the wrapper
+    animate: true, // Boolean: 是否启动CSS过渡效果（transitions）， true 或 false
+        transition: 400, // Integer: 过渡效果的执行速度，以毫秒（millisecond）为单位
+        label: "Menu", // String: Label for the navigation toggle
+        insert: "after", // String: Insert the toggle before or after the navigation
+        customToggle: "", // Selector: Specify the ID of a custom toggle
+        openPos: "relative", // String: Position of the opened nav, relative or static
+        jsClass: "js", // String: 'JS enabled' class which is added to <html> el
+        debug: false, // Boolean: Log debug messages to console, true 或 false
+        init: function(){}, // Function: Init callback
+    open: function(){}, // Function: Open callback
+    close: function(){} // Function: Close callback
+});
 
